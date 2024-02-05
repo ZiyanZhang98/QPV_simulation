013b1b91cb4abb4554ed6e9394661bfba17982cd
Ziyan Zhang <zy.zhang1129@gmail.com>
2024-02-05 23:57:03 +0100

    update network

--- BB84_eta_f.py
+++ BB84_eta_f.py
@@ -2,47 +2,19 @@
 import netsquid as ns
 import numpy as np
 from netsquid.protocols import NodeProtocol
-from netsquid.nodes import Node
+from netsquid.nodes import Node, Network
 import matplotlib.pyplot as plt
 from component import QuantumConnection, ClassicalConnection
 from bool_function import bool_func
 from verifier import V0Protocol, V1Protocol
+from prover import PProtocol
 # %%
-class PProtocol(NodeProtocol):
-    def __init__(self, node=None, name=None):
-        super().__init__(node, name)
-        self.x = None
-        self.y = None
-        self.port_q = self.node.ports['quantum']
-        self.port_c = self.node.ports['pv0']
-        self.port_c2 = self.node.ports['pv1']
-    
-    def run(self):
-        while True:
-            expr = yield (self.await_port_input(self.port_q)) or (yield (self.await_port_input(self.port_c) or self.await_port_input(self.port_c2)))
-
-            print(expr.first_term.first_term.value, expr.second_term.first_term.value, expr.second_term.second_term.value)
-
-            qubit = self.port_q.rx_input().items[0]
-            self.x = self.port_c.rx_input().items[0]
-            self.y = self.port_c2.rx_input().items[0]
-
-            is_q = expr.first_term.first_term.value
-            is_basis = (expr.second_term.first_term.value,expr.second_term.second_term.value)
-            print(is_q, is_basis)
-
-            if bool_func(self.x, self.y) == 0:
-                state, prob = ns.qubits.measure(qubit)
-                labels_z =  ("|0>", "|1>")
-                print(f"{ns.sim_time()}: Particle p received and measured "f"{labels_z[state]} with probability {prob:.2f}")
-            else:
-                state, prob = ns.qubits.measure(qubit, observable=ns.X)
-                labels_z =  ("|+>", "|->")
-                print(f"{ns.sim_time()}: Particle p received and measured "
-                    f"{labels_z[state]} with probability {prob:.2f}")
-            
-            self.port_c.tx_output(labels_z[state])
-            self.port_c2.tx_output(labels_z[state])
+def network_setup(d0p, d1p, d10):
+    node_v0 = Node('v0', port_names = ['quantum', 'v0p', 'v0v1'])
+    node_p = Node('p', port_names=['quantum', 'pv0', 'pv1'])
+    node_v1 = Node('v1', port_names=['v1p', 'v1v0'])
+    network = Network('QPV_BB84')
+    network.add_nodes([node_p, node_v0, node_v1])
 #%% Run the simulation
 def main(round, distance, x, y):
     fibre_distance = np.arange(1, distance)
